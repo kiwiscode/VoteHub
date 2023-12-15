e36bc67e86e8029df0f01f3dcaf1c9f8b61e09da
Aykut Kav <ayktkav@gmail.com>
2023-12-15 11:15:14 +0100

    done

--- server/config/index.js
+++ server/config/index.js
@@ -29,7 +29,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: "https://votehub-ih.netlify.app",
     })
   );
 
